5d3c36c92fe9cd9d08b9973f59f8b106457636c7
Brandon Desselle <desselle.brandon2@gmail.com>
2019-08-23 13:13:32 -0500

    Update BubblePage.js

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,13 +1,23 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-
+import React from "react";
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import { axiosWithAuth } from "../helpers/axiosWithAuth";
 
 const BubblePage = () => {
-  const [colorList, setColorList] = useState([]);
+  const [colorList, setColorList] = React.useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+
+  React.useEffect(() => {
+    axiosWithAuth()
+      .get("http://localhost:5000/api/colors")
+      .then(res => setColorList(res.data))
+      .catch(err => console.log(err));
+  }, []);
+
+  React.useEffect(() => {
+    console.log("there was a change why you no work");
+  }, [colorList]);
 
   return (
     <>
