596c197643f08d6552ed7428123837b1f65b41b4
Brandon Desselle <desselle.brandon2@gmail.com>
2019-08-23 13:13:38 -0500

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,50 @@
 import React from "react";
+import axios from "axios";
 
-const Login = () => {
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [currentValue, setCurrentValue] = React.useState({
+    username: "",
+    password: ""
+  });
+
+  function loginHandler(e) {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/api/login/", currentValue)
+      .then(res => {
+        console.log(res);
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/bubble");
+      })
+      .catch(err => console.log(err));
+  }
+
+  function handleChange(e) {
+    setCurrentValue({ ...currentValue, [e.target.name]: e.target.value });
+    console.log(currentValue);
+  }
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={loginHandler}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+        <button type="submit">Login</button>
+      </form>
     </>
   );
 };
